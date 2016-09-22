231c163136fc15a487cde502996dc51451ae5c5a
ftwdjw <ftwdjw@gmail.com>
2016-09-22 10:13:50 -0500

    initial commit. tested in xcode 8.

--- switch example/ViewController.swift
+++ switch example/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var switchButton: UISwitch!
+    
+    
+    @IBOutlet weak var labelText: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func valueChanged(_ sender: AnyObject) {
+        print(switchButton.isOn)//prints ture if switch is ON
+        if switchButton.isOn == true{
+            
+            labelText.text="the switch button is ON"
+            print("the switch button is on")
+           
+        }
+         else {print("the switch button is off")
+            labelText.text="the switch button is OFF" 
+        }
+    }
+    
 
 
 }
